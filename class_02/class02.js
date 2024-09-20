19a478f9e46f4fd6c5cc7845bc744a1ad5d63218
Sheikh amir <Sheikhamir036@gmail.com>
2024-09-20 22:15:24 +0530

    first and second class code

---
+++ class_02/class02.js
@@ -0,0 +1,58 @@
+"use strict";
+// console.log("working");
+
+// let a = 10;
+// console.log("i am first", a);
+// a = 12;
+// console.log("i am second", a);
+
+//  const , let , var
+
+// const a = "amir";
+// a = "amit";
+// let b = "dayan";
+// b = "rahul";
+
+// var c = "rahul";
+
+// console.log(a, b);
+
+/*
+Sure! Here’s the list of reserved keywords in JavaScript in a single row format:
+
+`break, case, catch, class, const, continue, debugger, default, delete, do, else, export, extends, finally, for, function, if, import, in, instanceof, new, return, super, switch, this, throw, try, typeof, var, void, while, with, yield, enum, implements, interface, let, package, private, protected, public, static, await, true, false, null, NaN, Infinity`
+*/
+
+// data types in javascript
+
+// const myNum = 87;
+// console.log(typeof myNum);
+
+// const myName = "amir";
+// console.log(typeof myName);
+
+// const myBoolean = false;
+// console.log(typeof myBoolean);
+
+// const myNull = null;
+// console.log(typeof myNull);
+
+// const myUndefined = undefined;
+// console.log(typeof myUndefined);
+
+//Interaction: alert, prompt, confirm
+
+// alert("thanks for subscribing");
+
+// const num = parseInt(prompt("Enter you first number for addition"));
+// const num2 = parseInt(prompt("Enter you second number for addition"));
+
+// if (isNaN(num) || isNaN(num2)) {
+//   alert("please enter number");
+//   //   console.log(num + num2);
+// } else {
+//   console.log(num + num2);
+// }
+
+// const result = confirm("Are you sure you are 18+");
+// console.log(result);
