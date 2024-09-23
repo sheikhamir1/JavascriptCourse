6acbedf4c8862f4ac0b843bf6189279519a48b0c
Sheikh amir <Sheikhamir036@gmail.com>
2024-09-23 22:26:51 +0530

    class03

---
+++ class_03/class_03.js
@@ -0,0 +1,56 @@
+// // type conversion
+
+// const a = "50";
+// const b = Number(a);
+
+// // console.log("converting string to number: ", b);
+// // console.log(typeof b);
+
+// const c = 20.8;
+// const d = String(c);
+
+// // console.log("converting number to string:", d);
+// // console.log(typeof d);
+
+// const e = true;
+// const f = String(e);
+
+// // console.log("converting boolean to string:", f);
+// // console.log(typeof f);
+
+// const g = 110;
+// const h = Boolean(g);
+
+// // console.log("converting number to boolean:", h);
+// // console.log(typeof h);
+
+// const i = "amir";
+// const j = Boolean(i);
+
+// console.log("converting number to boolean:", j);
+// console.log(typeof j);
+
+// let a = "10a" - 5;
+// console.log(a);
+// console.log(typeof a);
+// a = 45;
+// console.log(a);
+// console.log(typeof a);
+
+// const num = 10 + 5;
+// console.log(num);
+
+// const str = "amir" + "khan";
+// console.log(str);
+
+// const num = 2 + 2 + "1" + (2 + 2);
+// console.log(num);
+
+// let num1 = 2 + 2;
+// const num3 = "1";
+// num1 = 5 + 5;
+// console.log(num1 + num3 + num1);
+
+// const num = (2 + "1b") * (3 * 2);
+// // const num = 2 + "1" - 3 * 2;
+// console.log(num);
