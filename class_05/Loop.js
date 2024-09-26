1b47935c085c20ead50bf6ea5221bc206d045264
Sheikh amir <Sheikhamir036@gmail.com>
2024-09-26 22:06:04 +0530

    class05

---
+++ class_05/Loop.js
@@ -0,0 +1,32 @@
+// lets learn loop
+
+// const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// console.log(array.length);
+
+// for (let i = 0; i < array.length; i++) {
+//   const element = array[i];
+//   console.log(element);
+// }
+
+// for (let i = array.length - 1; i >= 0; i--) {
+//   const element = array[i];
+//   console.log(element);
+// }
+
+// let a = 1;
+
+// while (a <= 50) {
+//   console.log("index", a);
+//   a++;
+// }
+// console.log("value of a", a);
+
+// const obj = {
+//   name: "amir",
+//   age: 20,
+// };
+
+// for (let key in obj) {
+//   console.log(key);
+//   //   console.log(key, obj[key]);
+// }
---
+++ class_05/ifElse.js
@@ -0,0 +1,21 @@
+// console.log("this is class 05");
+
+// const a = 10;
+// const b = 20;
+// const c = 30;
+
+// if (a > b || b < c) {
+//   console.log("first if block excuted");
+// } else if (a > c) {
+//   console.log("second if block excuted");
+// } else if (a > c) {
+//   console.log("third if block excuted");
+// } else {
+//   console.log("else block excuted");
+// }
+
+// if (!true) {
+//   console.log("its true");
+// } else {
+//   console.log("its false");
+// }
