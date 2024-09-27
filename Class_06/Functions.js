a1d609edae6bf81642dc5f474783d35b32cc2dcd
Sheikh amir <Sheikhamir036@gmail.com>
2024-09-27 22:18:05 +0530

    class06

---
+++ Class_06/Functions.js
@@ -0,0 +1,44 @@
+// lets learn functions
+
+// function ShowAlert() {
+//   const x = 10;
+//   //   let x = 20;
+
+//   console.log("console in the function block", x);
+// }
+
+// ShowAlert();
+// // x = 100;
+
+// console.log("console in the global block ", x);
+// let xx = 100;
+// console.log("console in the global block", thisIsVar);
+
+// const a = 10;
+// let b = 20;
+// if (true) {
+//   let b = 50;
+//   console.log("b in local scope", b);
+// }
+
+// console.log(b);
+
+// var c = 30;
+// var c = 70;
+// console.log(c);
+
+// var date =
+
+// let date = new Date();
+// const newDate = date.toLocaleString();
+
+// console.log(newDate);
+
+// let marks = 80;
+// marks += 2;
+// marks -= 10;
+// console.log(marks);
+
+// what is the fact of 7
+
+//  1*2*3*4*5*6*7
